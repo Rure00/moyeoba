ba62112c1b305fa5317a902b283224b7171b4e2e
Rure00 <asnjkd@naver.com>
2024-02-23 12:33:58 +0900

    Add KakaoLogin

---
+++ project/src/main/java/com/moyeoba/project/api/kakao/KakaoProfileDto.java
@@ -0,0 +1,13 @@
+package com.moyeoba.project.api.kakao;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class KakaoProfileDto {
+    private Long id;
+
+    //Filed Info Docs
+    //https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#kakaoaccount
+}
