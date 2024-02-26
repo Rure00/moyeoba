55f0bf5905eddd6ee8f16e97706a78cfa21def1c
Rure00 <asnjkd@naver.com>
2024-02-26 16:11:19 +0900

    Ignore ProjectApplicationTests

--- project/src/main/java/com/moyeoba/project/ProjectApplication.java
+++ project/src/main/java/com/moyeoba/project/ProjectApplication.java
@@ -11,6 +11,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories
 public class ProjectApplication {
 
+	//
+
 	//https://velog.io/@jonghyun3668/SpringBoot-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8-EC2-%EB%B0%B0%ED%8F%AC%ED%95%98%EA%B8%B0
 
 	public static void main(String[] args) {
