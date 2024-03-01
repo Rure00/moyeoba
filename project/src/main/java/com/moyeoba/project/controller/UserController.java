40df4c6814f22b60d01714e692bc215a572fd13a
Rure00 <asnjkd@naver.com>
2024-03-01 14:47:25 +0900

    integrate login_controller in user_controller

--- project/src/main/java/com/moyeoba/project/controller/UserController.java
+++ project/src/main/java/com/moyeoba/project/controller/UserController.java
@@ -1,17 +1,19 @@
 package com.moyeoba.project.controller;
 
+import com.moyeoba.project.data.dto.request.LoginRequestDto;
 import com.moyeoba.project.service.KakaoService;
 import com.moyeoba.project.service.NaverService;
 import com.moyeoba.project.token.data.TokenPair;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @Slf4j
 @RestController
@@ -33,5 +35,46 @@ public class UserController {
         log.info("SignUp: ");
 
         return new ResponseEntity<>("", HttpStatus.OK);
+    }
+
+    @PostMapping("/-")
+    public ResponseEntity<TokenPair> integratedLogin(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        log.info("Login");
+        String type = loginRequestDto.getType();
+        String token = loginRequestDto.getToken();
+
+        if(!Objects.equals(type, "kakao") && !Objects.equals(type, "naver")) {
+            log.info("Login: Wrong parameter Passed.");
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+
+        log.info("Try login with {}", type);
+        TokenPair tokenPair = null;
+        try {
+            if(Objects.equals(type, "kakao")) tokenPair = kakaoService.kakaoTokenLogin(token);
+            else tokenPair = naverService.naverTokenLogin(token);
+
+            if(tokenPair == null)
+                return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+
+            Cookie accessCookie = new Cookie("access_token", tokenPair.getAccessToken());
+            Cookie refreshCookie = new Cookie("refresh_token", tokenPair.getRefreshToken());
+
+            accessCookie.setPath("/login/-");
+            accessCookie.setMaxAge(60 * 30);
+            accessCookie.setDomain("localhost");    //TODO: "moyeoba.com" 로 바꾸기
+
+            refreshCookie.setPath("/login/-");
+            refreshCookie.setMaxAge(60 * 60 * 24 * 14);
+            refreshCookie.setDomain("localhost");       //TODO: "moyeoba.com" 로 바꾸기
+
+            response.addCookie(accessCookie);
+            response.addCookie(refreshCookie);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(tokenPair, HttpStatus.OK);
     }
 }
