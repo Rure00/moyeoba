0aa023e29f505b5eaa5c1159ed4e7d25ccc603fd
Rure00 <asnjkd@naver.com>
2024-03-03 16:43:46 +0900

    create a kotlin project

---
+++ src/main/kotlin/com/moyeoba/moyeoba/MoyeobaApplication.kt
@@ -0,0 +1,11 @@
+package com.moyeoba.moyeoba
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MoyeobaApplication
+
+fun main(args: Array<String>) {
+	runApplication<MoyeobaApplication>(*args)
+}
