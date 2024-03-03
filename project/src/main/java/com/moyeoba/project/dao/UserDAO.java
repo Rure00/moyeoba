198c2f9526eb57c7ece0d41a79ec4a91b7661039
Rure00 <asnjkd@naver.com>
2024-03-03 16:07:16 +0900

    Delete an ex-project

--- project/src/main/java/com/moyeoba/project/dao/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.moyeoba.project.dao;
-
-import com.moyeoba.project.data.entity.User;
-
-public interface UserDAO {
-    User getUserById(Long id);
-    User getUserByNaverId(String id);
-    User getUserByKakaoId(Integer id);
-}
