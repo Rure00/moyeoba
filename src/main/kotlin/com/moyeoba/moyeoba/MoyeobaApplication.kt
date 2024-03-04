f553321f165916d7af42ee5f99c81267b5026c7d
Rure00 <asnjkd@naver.com>
2024-03-05 00:54:26 +0900

    set basic dependencies.

--- src/main/kotlin/com/moyeoba/moyeoba/MoyeobaApplication.kt
+++ src/main/kotlin/com/moyeoba/moyeoba/MoyeobaApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class MoyeobaApplication
+class MoyeobaApplication {}
 
 fun main(args: Array<String>) {
 	runApplication<MoyeobaApplication>(*args)
