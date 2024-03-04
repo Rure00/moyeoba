256293f359543e4cb9cec2e919f44b3f26b73d50
Rure00 <asnjkd@naver.com>
2024-03-04 21:52:57 +0900

    Create a new kotlin project

---
+++ src/test/kotlin/com/moyeoba/moyeoba/MoyeobaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.moyeoba.moyeoba
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoyeobaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
