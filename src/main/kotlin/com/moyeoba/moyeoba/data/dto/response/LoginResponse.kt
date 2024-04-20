6aa951085599050f49c26258e2cf25bdd093d1a1
Rure00 <asnjkd5193@gmail.com>
2024-04-20 14:53:04 +0900

    set jsonproperty on LoginResponse.isNewUser Because is-prfix is deleted.

--- src/main/kotlin/com/moyeoba/moyeoba/data/dto/response/LoginResponse.kt
+++ src/main/kotlin/com/moyeoba/moyeoba/data/dto/response/LoginResponse.kt
@@ -1,5 +1,9 @@
 package com.moyeoba.moyeoba.data.dto.response
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class LoginResponse(
+        @get:JsonProperty("isNewUser")
+        @param:JsonProperty("isNewUser")
         val isNewUser: Boolean
 )
