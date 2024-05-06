e360d66e5e1886bedc19612a4e47d00ac026064b
Rure00 <asnjkd5193@gmail.com>
2024-05-06 17:58:08 +0900

    remove configue about channel

--- src/main/kotlin/com/moyeoba/moyeoba/web_socket/StompWebSocketConfig.kt
+++ src/main/kotlin/com/moyeoba/moyeoba/web_socket/StompWebSocketConfig.kt
@@ -3,6 +3,7 @@ package com.moyeoba.moyeoba.web_socket
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.simp.config.ChannelRegistration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
@@ -24,11 +25,27 @@ class StompWebSocketConfig: WebSocketMessageBrokerConfigurer {
         registry.enableSimpleBroker("/sub") // 브로커 -> 구독자들(메세지받을때)
     }
 
-
+/*
     override fun configureClientInboundChannel(registration: ChannelRegistration) {
         println("Configuring task executor for Client Inbound Channel")
         registration.taskExecutor().corePoolSize(4)
         registration.taskExecutor().maxPoolSize(4)
+
+        val executor = ThreadPoolTaskExecutor()
+        executor.corePoolSize = 2
+        executor.maxPoolSize = 2
+        executor.initialize()
+
+        registration.taskExecutor(executor)
+
     }
+
+    override fun configureClientOutboundChannel(registration: ChannelRegistration) {
+        super.configureClientOutboundChannel(registration)
+        registration.taskExecutor().corePoolSize(4)
+        registration.taskExecutor().maxPoolSize(4)
+    }
+
+ */
 
 }
