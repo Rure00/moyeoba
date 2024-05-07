9c9dfec94f9022ff6eb64195a8af595b2238ccf6
Rure00 <asnjkd5193@gmail.com>
2024-05-07 16:42:07 +0900

    Add sendTo annotation.

--- src/main/kotlin/com/moyeoba/moyeoba/controller/ChatController.kt
+++ src/main/kotlin/com/moyeoba/moyeoba/controller/ChatController.kt
@@ -18,7 +18,7 @@ class ChatController {
     private lateinit var simpleMessage: SimpMessageSendingOperations
 
     @MessageMapping("/hello") //여기로 전송되면 메서드 호출 -> WebSocketConfig prefixes 에서 적용한건 앞에 생략
-    //@SendTo("/room/{roomId}") //구독하고 있는 장소로 메시지 전송 (목적지)  -> WebSocketConfig Broker 에서 적용한건 앞에 붙어줘야됨
+    @SendTo("/sub") //구독하고 있는 장소로 메시지 전송 (목적지)  -> WebSocketConfig Broker 에서 적용한건 앞에 붙어줘야됨
     fun chat(@DestinationVariable roomId: Long?, message: Message): Message? {
         //채팅 저장
 
