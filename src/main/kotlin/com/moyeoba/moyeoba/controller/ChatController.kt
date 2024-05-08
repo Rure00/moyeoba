42411478638ab0e290c3f8b0bc59ba559448b0b2
Rure00 <asnjkd5193@gmail.com>
2024-05-08 12:31:54 +0900

    1

--- src/main/kotlin/com/moyeoba/moyeoba/controller/ChatController.kt
+++ src/main/kotlin/com/moyeoba/moyeoba/controller/ChatController.kt
@@ -19,7 +19,7 @@ class ChatController {
     private lateinit var simpleMessage: SimpMessageSendingOperations
 
     @MessageMapping("hello") //여기로 전송되면 메서드 호출 -> WebSocketConfig prefixes 에서 적용한건 앞에 생략
-    @SendToUser("/sub/hello") //구독하고 있는 장소로 메시지 전송 (목적지)  -> WebSocketConfig Broker 에서 적용한건 앞에 붙어줘야됨
+    @SendTo("/sub/hello") //구독하고 있는 장소로 메시지 전송 (목적지)  -> WebSocketConfig Broker 에서 적용한건 앞에 붙어줘야됨
     fun chat(@DestinationVariable roomId: Long?, message: Message) {
         //채팅 저장
         println("Get Message) ${message.sender} to ${message.receiver}: ${message.data}")
