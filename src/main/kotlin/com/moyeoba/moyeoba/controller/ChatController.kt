44c48335e15eed0cb2a82cd849da5d0bd9f6ff85
Rure00 <asnjkd5193@gmail.com>
2024-05-14 15:32:27 +0900

    add StompConfig to .gitignore and add return message.

--- src/main/kotlin/com/moyeoba/moyeoba/controller/ChatController.kt
+++ src/main/kotlin/com/moyeoba/moyeoba/controller/ChatController.kt
@@ -21,8 +21,9 @@ class ChatController {
 
     @MessageMapping("/hello") //여기로 전송되면 메서드 호출 -> WebSocketConfig prefixes 에서 적용한건 앞에 생략
     @SendTo("/sub/hello") //구독하고 있는 장소로 메시지 전송 (목적지)  -> WebSocketConfig Broker 에서 적용한건 앞에 붙어줘야됨
-    fun chat(@Payload message: Message) {
+    fun chat(@Payload message: Message): Message {
         //채팅 저장
         println("Get Message) ${message.sender} to ${message.receiver}: ${message.data}")
+        return message
     }
 }
