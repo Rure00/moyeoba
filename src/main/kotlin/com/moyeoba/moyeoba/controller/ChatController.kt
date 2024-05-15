902a659152f342e96164ed93838cad35dfadb558
Rure00 <asnjkd5193@gmail.com>
2024-05-15 15:41:59 +0900

    change class: Message to make it json.

--- src/main/kotlin/com/moyeoba/moyeoba/controller/ChatController.kt
+++ src/main/kotlin/com/moyeoba/moyeoba/controller/ChatController.kt
@@ -1,7 +1,10 @@
 package com.moyeoba.moyeoba.controller
 
+
 import com.moyeoba.moyeoba.data.dto.Message
 import com.moyeoba.moyeoba.service.ChatService
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.messaging.handler.annotation.DestinationVariable
 import org.springframework.messaging.handler.annotation.MessageMapping
@@ -20,10 +23,12 @@ class ChatController {
     private lateinit var simpleMessage: SimpMessageSendingOperations
 
     @MessageMapping("/hello") //여기로 전송되면 메서드 호출 -> WebSocketConfig prefixes 에서 적용한건 앞에 생략
-    @SendTo("/sub/hello") //구독하고 있는 장소로 메시지 전송 (목적지)  -> WebSocketConfig Broker 에서 적용한건 앞에 붙어줘야됨
-    fun chat(@Payload message: Message): Message {
+    //@SendTo("/sub/hello") //구독하고 있는 장소로 메시지 전송 (목적지)  -> WebSocketConfig Broker 에서 적용한건 앞에 붙어줘야됨
+    fun chat(message: Message): Message {
+        val roomId = 0L
         //채팅 저장
-        println("Get Message) ${message.sender} to ${message.receiver}: ${message.data}")
+        println("Get Message) $message")
+        simpleMessage.convertAndSend("/sub/${roomId}", Json.encodeToString(message))
         return message
     }
 }
--- src/main/kotlin/com/moyeoba/moyeoba/data/dto/Message.kt
+++ src/main/kotlin/com/moyeoba/moyeoba/data/dto/Message.kt
@@ -1,17 +1,22 @@
 package com.moyeoba.moyeoba.data.dto
 
+import com.fasterxml.jackson.databind.util.JSONPObject
+import kotlinx.serialization.Serializable
 import org.springframework.messaging.Message
 import org.springframework.messaging.MessageHeaders
 
+@Serializable
 data class Message(
     var type: String,
     var sender: String,
     var receiver: String,
     var data: String,
-) {
+){
     constructor(sender: String, receiver: String):
             this("", sender = sender,receiver = receiver, "")
     constructor(): this(
         "", sender = "",receiver = "", ""
     )
+
+
 }
