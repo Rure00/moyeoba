c2158142be6b4e478b9482a5550a2813fde51adf
Rure00 <asnjkd5193@gmail.com>
2024-06-04 20:30:40 +0900

    add a missing jpaRepository<CatMessage, Long>

--- src/main/kotlin/com/moyeoba/moyeoba/repository/ChatRepository.kt
+++ src/main/kotlin/com/moyeoba/moyeoba/repository/ChatRepository.kt
@@ -2,10 +2,11 @@ package com.moyeoba.moyeoba.repository
 
 import com.moyeoba.moyeoba.data.entity.ChatMessage
 import com.moyeoba.moyeoba.data.entity.User
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
-interface ChatRepository {
+interface ChatRepository: JpaRepository<ChatMessage, Long> {
     fun insert(chatMessage: ChatMessage): ChatMessage
 }
