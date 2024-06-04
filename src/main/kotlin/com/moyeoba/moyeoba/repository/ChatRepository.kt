e41391098f33562ecdd2e5e5129b7ab2415254c7
Rure00 <asnjkd5193@gmail.com>
2024-06-04 20:27:08 +0900

    add annotation: @Repository at ChatRepository

--- src/main/kotlin/com/moyeoba/moyeoba/repository/ChatRepository.kt
+++ src/main/kotlin/com/moyeoba/moyeoba/repository/ChatRepository.kt
@@ -2,8 +2,10 @@ package com.moyeoba.moyeoba.repository
 
 import com.moyeoba.moyeoba.data.entity.ChatMessage
 import com.moyeoba.moyeoba.data.entity.User
+import org.springframework.stereotype.Repository
 import java.util.*
 
+@Repository
 interface ChatRepository {
     fun insert(chatMessage: ChatMessage): ChatMessage
 }
